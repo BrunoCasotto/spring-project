58bec122310eaea7e8e5b33949fa4d228986f138
Bruno Casotto <casottoalves@hotmail.com>
2017-11-10 14:58:17 -0200

    post added

--- src/main/java/com/spring/project/springproject/controllers/ProductController.java
+++ src/main/java/com/spring/project/springproject/controllers/ProductController.java
@@ -4,15 +4,26 @@ import com.spring.project.springproject.models.Product;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 
 @RestController
 public class ProductController {
 
     private Product product;
 
-    @RequestMapping("/casotto")
-    public Product index() {
+    @RequestMapping(value = "product", method=GET)
+    public Product getProduct() {
         product = new Product("CasottoProduct", 1300.00);
+        System.out.println("calling GET");
+        return product;
+    }
+
+    @RequestMapping(value = "product", method=POST)
+    public Product saveProduct() {
+        product = new Product("CasottoProduct", 1300.00);
+        System.out.println("calling POST");
         return product;
     }
 }
